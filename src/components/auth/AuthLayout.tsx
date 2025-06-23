ed9b26b3a8f1627722ee93d5dea7bc5ebe43d55c
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-06-23 23:41:27 +0200

    Adapt Login UI to iPad screen size

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -7,7 +7,7 @@ type AuthLayoutProps = {
 function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <>
-      <div className="flex h-full flex-col items-center justify-center bg-[#f9fafb] px-[1.625rem] py-12 dark:bg-gradient-to-t dark:from-[#000000] dark:to-[#0f0f0f]">
+      <div className="flex h-full flex-col items-center justify-center bg-[#f9fafb] px-[1.625rem] pb-12 dark:bg-gradient-to-t dark:from-[#000000] dark:to-[#0f0f0f] sm:pt-6 xs:pt-6">
         {children}
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center border-t bg-white pb-4 pt-4 text-sm text-[#17182399] dark:border-[#424242] dark:bg-[#212121] standalone:pb-6">
